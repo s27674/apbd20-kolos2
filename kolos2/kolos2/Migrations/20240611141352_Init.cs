23877b31929a736ab11a2d18ad96c15e080ad526
s27674 <s27674@pjwstk.edu.pl>
2024-06-11 17:17:07 +0200

    kolos

---
+++ kolos2/kolos2/Migrations/20240611141352_Init.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace kolos2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Character",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    CurrentWeight = table.Column<int>(type: "int", nullable: false),
+                    MaxWeight = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Character", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    IdItems = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.IdItems);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Title",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Title", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Backpack",
+                columns: table => new
+                {
+                    CharacterId = table.Column<int>(type: "int", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Backpack", x => new { x.CharacterId, x.ItemId });
+                    table.ForeignKey(
+                        name: "FK_Backpack_Character_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Character",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Backpack_Item_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Item",
+                        principalColumn: "IdItems",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Character_titels",
+                columns: table => new
+                {
+                    CharacterId = table.Column<int>(type: "int", nullable: false),
+                    TitelsId = table.Column<int>(type: "int", nullable: false),
+                    AcquiredAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Character_titels", x => new { x.CharacterId, x.TitelsId });
+                    table.ForeignKey(
+                        name: "FK_Character_titels_Character_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Character",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Character_titels_Title_TitelsId",
+                        column: x => x.TitelsId,
+                        principalTable: "Title",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Character",
+                columns: new[] { "Id", "CurrentWeight", "FirstName", "LastName", "MaxWeight" },
+                values: new object[,]
+                {
+                    { 1, 14, "FirstName1", "LastName1", 40 },
+                    { 2, 15, "FirstName2", "LastName12", 50 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Item",
+                columns: new[] { "IdItems", "Name", "Weight" },
+                values: new object[,]
+                {
+                    { 1, "ItemName1", 0 },
+                    { 2, "ItemName2", 0 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Title",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "TitlesName1" },
+                    { 2, "TitlesName2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Backpack",
+                columns: new[] { "CharacterId", "ItemId", "Amount" },
+                values: new object[,]
+                {
+                    { 1, 1, 11 },
+                    { 2, 2, 22 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Character_titels",
+                columns: new[] { "CharacterId", "TitelsId", "AcquiredAt" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2022, 12, 30, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, 2, new DateTime(2021, 2, 14, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Backpack_ItemId",
+                table: "Backpack",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Character_titels_TitelsId",
+                table: "Character_titels",
+                column: "TitelsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Backpack");
+
+            migrationBuilder.DropTable(
+                name: "Character_titels");
+
+            migrationBuilder.DropTable(
+                name: "Item");
+
+            migrationBuilder.DropTable(
+                name: "Character");
+
+            migrationBuilder.DropTable(
+                name: "Title");
+        }
+    }
+}
---
+++ kolos2/kolos2/Program.cs
@@ -0,0 +1,28 @@
+using kolos2.Data;
+using kolos2.Services;
+using kolos2.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ItemsContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddScoped<IDbService, DbService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
